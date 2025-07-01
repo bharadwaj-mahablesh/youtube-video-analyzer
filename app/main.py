592b9bbf5f935905a255b897e4eae88352d2a680
Bharadwaj Mahablesh <bharadwaj.mahablesh@gmail.com>
2025-07-02 00:45:08 +0530

    refactor:fixing vercel build issues

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from app.api.routes import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.include_router(api_router)
 
